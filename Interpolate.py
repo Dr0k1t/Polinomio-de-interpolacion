e6ba5e5bea7ecc8200d3d25b2bd75885c1cd7f6e
Tostón <54345339+Dr0k1t@users.noreply.github.com>
2020-10-07 04:29:38 -0500

    Update Interpolate.py

--- Interpolate.py
+++ Interpolate.py
@@ -9,18 +9,10 @@ def _interpolate(p):
     n = p.copy()
     z_i = []
     for i in range(len(n)):
-        z_i.append(math.pow(n[i],i))
+        z_i.append(math.pow(n[i],i)) 
     return la.solve(np.vander(n), z_i)
 
-intervalo = np.linspace(-1,1,101)
-vander_matrix = np.vander(intervalo)
-
-
-print("Número de condición 1: ", la.cond(vander_matrix,1))
-print("Número de condición 2: ", la.cond(vander_matrix,2))
-print("Número de condición infinito: ", la.cond(vander_matrix, np.infty))
-pprint(vander_matrix)
+intervalo = np.linspace(-1,1,101) #Un conjunto de 101 datos igualmente distribuidos entre -1 y 1 
 
 pl.plot(intervalo, _interpolate(intervalo))
-pl.savefig("interpolacion")
 pl.show()
